56d378ded18c44af71bcb28351b687d13ddbfb1a
Debanjan Saha <debanjansaha2001@gmail.com>
2022-10-29 15:21:10 +0530

    Finished a dew functionsin BST

--- asgn-4/p2/p2.go
+++ asgn-4/p2/p2.go
@@ -22,6 +22,8 @@ type MyBST struct {
 	Root 		*TreeNode 	`json:"Root"`
 }
 
+const pk = "19CS30014-19CS10048"
+
 func (s* SmartContract)Insert(ctx contractapi.TransactionContextInterface, val int) error {
 	bst, err := s.ReadMyBST(ctx)
 	if err != nil {
@@ -29,42 +31,49 @@ func (s* SmartContract)Insert(ctx contractapi.TransactionContextInterface, val i
 	}
 
 	if bst != nil {
-		err := s.UpdateMyBST(ctx, val, MyBST, InsertValue())
+		err := s.UpdateMyBST(ctx, val, bst, 0)
 		if err != nil {
 			return err
 		}
 		return nil 
 	}
 
-
 	var node = TreeNode{
-		val: val,
+		Val: val,
 		Left: nil,
 		Right: nil,
 	} 
 
-	bst_pk := "19CS30014-19CS10048"
-	var bst = MyBST{
+	bst_pk := pk
+	var mybst = MyBST{
 		PrimaryKey: bst_pk,
 		Root: &node,
 	}
 
-	bstJSON, err := json.Marshal(bst)
+	bstJSON, err := json.Marshal(mybst)
 	if err != nil {
 		return err
 	}
 	return ctx.GetStub().PutState(bst_pk, bstJSON) 
 }
 
-func Delete(ctx contractapi.TransactionContextInterface, val int) error {
+func (s* SmartContract)Delete(ctx contractapi.TransactionContextInterface, val int) error {
 	bst, err := s.ReadMyBST(ctx)
 	if err != nil {
 		return err
 	}
 
-	if bst != nil {
-		
+	if bst == nil {
+		return fmt.Errorf("The tree is not found")
 	}
+
+	err = s.UpdateMyBST(ctx, val, bst, 1)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 
@@ -73,7 +82,6 @@ func Preorder(ctx contractapi.TransactionContextInterface) (string, error) {
 }
 
 func Inorder(ctx contractapi.TransactionContextInterface) (string, error) {
-
 }
 
 func TreeHeight(ctx contractapi.TransactionContextInterface) (string, error) {
@@ -84,29 +92,64 @@ func MyBSTExists(ctx contractapi.TransactionContextInterface, key string) (bool,
 
 }
 
-func ReadMyBST(ctx contractapi.TransactionContextInterface) (*MyBST, error) {
+func (s* SmartContract)ReadMyBST(ctx contractapi.TransactionContextInterface) (*MyBST, error) {
+	bstJSON, err := ctx.GetStub().GetState(pk)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read from world state: %v", err)
+	}
+
+	if bstJSON == nil {
+		return nil, fmt.Errorf("The bst with primary key %s does not exist", pk)
+	}
+
+	var bst *MyBST
+	err = json.Unmarshal(bstJSON, &bst)
+	if err != nil {
+	  return nil, err
+	}
 	
+	return bst, nil
 }
 
-func UpdateMyBST(ctx contractapi.TransactionContextInterface, val int, bst *MyBST, operation int) error {
+func (s* SmartContract)UpdateMyBST(ctx contractapi.TransactionContextInterface, val int, bst *MyBST, operation int) error {
 
+	if operation == 0 {
+
+	}else if operation == 1{
+
+	}else {
+		return fmt.Errorf("Illegal Operation in UpdateMyBST()")
+	}
 }
 
-func InsertValue() int {
-	return 0	
+func InsertValue(bst *MyBST, val int) error {
+
+	return nil
 }
 
-func DeleteValue() int {
-	return 1
+func DeleteValue(bst *MyBST, val int) error {
+	return nil
 }
 
 
-func preorderTraversal() {
+func preorderTraversal(node *TreeNode, order []int) {
+	if node == nil {
+		return 
+	}
 
+	order = append(order, node.Val)
+	preorderTraversal(node.Left, order)
+	preorderTraversal(node.Right, order)
 }
 
-func inorderTraversal() {
+func inorderTraversal(node *TreeNode, order []int) {
+	if node == nil {
+		return 
+	}
 
+	inorderTraversal(node.Left, order)
+	order = append(order, node.Val)
+	inorderTraversal(node.Right, order)
 }
 
 func heightOfTree() {
